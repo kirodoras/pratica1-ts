533af018a61675ccd5163b97f80d9fb7000fed02
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-31 20:22:47 -0300

    fix: raking search

--- src/repositories/ranking.repository.ts
+++ src/repositories/ranking.repository.ts
@@ -1,6 +1,6 @@
 import { connection } from "../database.js";
 
 export async function select(){
-    const result = await connection.query(`SELECT username, wins, losses, draws FROM fighters`);
+    const result = await connection.query(`SELECT username, wins, losses, draws FROM fighters ORDER BY wins DESC`);
     return result.rows;
 }
